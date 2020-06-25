76469ba372a39f38e8509805c17130ce42d484a6
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-25 14:36:41 -0400

    Finished the backend flow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @available_tickets = Ticket.all
+    @all_tickets = Ticket.all.map { |ticket| ticket.name }
     @ticket = Ticket.new
+
+    # @selected_name = @all_tickets.delete_at(rand(@all_tickets.length))
   end
 end
