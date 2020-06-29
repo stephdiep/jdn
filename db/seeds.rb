6d3a13bb3a898aca9bcdfd048be744c5ca748431
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-29 18:06:45 -0400

    Coded the home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-names = ['Aria', 'Kent', 'Brenda', 'Christine', 'Thu Hong', 'Lan']
+# names = ['Aria', 'Kent', 'Brenda', 'Christine', 'Thu Hong', 'Lan']
 
-names.each do |name|
-  Ticket.create!(name: name)
-end
+# names.each do |name|
+#   Ticket.create!(name: name)
+# end
 
-puts("Created all tickets.")
+# puts("Created all tickets.")
+
+User.create!(name: "Stephanie", email: "steph@jdn.org", password: "stephane", admin: true)
+User.create!(name: "Christine", email: "chris@jdn.org", password: "christian", admin: true)
+User.create!(name: "Brenda", email: "bren@jdn.org", password: "brandon", admin: true)
