384f5d27fd769c81168d90e5bf2a445e06e2a172
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-30 11:40:10 -0400

    Added more ux-friendly features on contribution form

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,6 @@
 class TicketsController < ApplicationController
   def index
-    @all_tickets = Ticket.all.order(:created_at).map { |ticket| ticket.name }
+    @all_tickets = Ticket.all.order(:name).map { |ticket| ticket.name }
     @ticket = Ticket.new
     @last_winner = Ticket.where(winner: true).order(updated_at: :desc).first
     @winners = Ticket.where(winner: true).order(updated_at: :desc).reject do |ticket|
