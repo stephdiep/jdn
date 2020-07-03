2a88f55c42657c1c009f1b14f71be8953dc03ac4
Stephanie Diep <stephanie.diep002@gmail.com>
2020-07-03 15:05:49 -0400

    Uncommented seeds file to push to production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# User.create!(name: "Stephanie", email: "steph@jdn.org", password: "stephane", admin: true)
-# User.create!(name: "Christine", email: "chris@jdn.org", password: "christian", admin: true)
-# User.create!(name: "Brenda", email: "bren@jdn.org", password: "brandon", admin: true)
-# puts("All users created.")
+User.create!(name: "Stephanie", email: "steph@jdn.org", password: "stephane", admin: true)
+User.create!(name: "Christine", email: "chris@jdn.org", password: "christian", admin: true)
+User.create!(name: "Brenda", email: "bren@jdn.org", password: "brandon", admin: true)
+puts("All users created.")
 
-# Cash.create!(value: 251.95, description: 'Fonds initial investi 2019', category: 'credit')
-# Cash.create!(value: 92.71, description: 'Restant JdN19', category: 'credit')
-# puts("All cashes created.")
+Cash.create!(value: 251.95, description: 'Fonds initial investi 2019', category: 'credit')
+Cash.create!(value: 92.71, description: 'Restant JdN19', category: 'credit')
+puts("All cashes created.")
 
